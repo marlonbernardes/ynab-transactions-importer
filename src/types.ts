f789f698174ee04853149b152d0ed6ec4949e088
Marlon Bernardes <marlon@getimpala.com>
2020-07-08 10:14:19 +0100

    feat(*): change string types into enums

--- src/types.ts
+++ src/types.ts
@@ -9,24 +9,24 @@ export type CliOptions = {
 }
 
 export type YnabTransaction = {
-  date?: string
-  amount?: number
+  date: string
+  amount: number
   payee_id?: string
   payee_name?: string
   account_id?: string
   category_id?: string
   memo?: string
-  cleared?: string
+  cleared?: 'cleared' | 'uncleared' | 'reconciled'
   approved?: boolean
-  flag_color?: string
+  flag_color?: 'red' | 'orange' | 'yellow' | 'green' | 'blue' | ' purple'
   import_id?: string
   subtransactions?: YnabSubTransaction[]
 }
 
 export type YnabSubTransaction = {
   amount: number
-  payee_id: string
-  payee_name: string
-  category_id: string
-  memo: string
+  payee_id?: string
+  payee_name?: string
+  category_id?: string
+  memo?: string
 }
